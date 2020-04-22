d6dea54fdbfb4ec7c0c1fc9e9eebee020a7fd9e7
Taishawn King <taishawnking1@yahoo.com>
2020-04-22 05:14:02 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,5 +1,5 @@
 UPDATE species 
-SET species = 'The Hobbit' WHERE characters = "cylon";
+SET characters = 'The Hobbit' WHERE characters = "cylon";
 
 UPDATE table_name
 SET column1 = value1, column2 = value2, ...
