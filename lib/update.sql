0750eb9de1ea77c2464d02a4469975111dba76ce
Taishawn King <taishawnking1@yahoo.com>
2020-04-22 05:13:29 +0000

    Automatically backed up by Learn

--- lib/update.sql
+++ lib/update.sql
@@ -1,1 +1,6 @@
-UPDATE species SET species.characters = 'The Hobbit' WHERE characters = "cylon";
+UPDATE species 
+SET species = 'The Hobbit' WHERE characters = "cylon";
+
+UPDATE table_name
+SET column1 = value1, column2 = value2, ...
+WHERE condition;
